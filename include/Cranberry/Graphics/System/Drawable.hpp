b0400bfe4881c1a029a759c85505329315e4008d
Nicolas Kogler <nicolas.kogler@hotmail.com>
2017-02-13 22:41:37 +0100

    Add the base class for all graphics objects.

---
+++ include/Cranberry/Graphics/System/Drawable.hpp
@@ -0,0 +1,186 @@
+﻿//
+//  cranberry: C++ game engine using the Qt framework and OpenGL/ES.
+//  Copyright (C) 2017 Nicolas Kogler
+//
+//  Cranberry is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Cranberry is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with cranberry. If not, see <http://www.gnu.org/licenses/>.
+//
+
+
+#pragma once
+#ifndef CRANBERRY_DRAWABLE_HPP
+#define CRANBERRY_DRAWABLE_HPP
+
+
+// Cranberry headers
+#include <Cranberry/System/GameTime.hpp>
+#include <Cranberry/Graphics/System/GraphicsEnums.hpp>
+#include <Cranberry/Window/Window.hpp>
+
+// Forward declarations
+class QOpenGLBuffer;
+class QOpenGLContext;
+class QOpenGLFunctions;
+class QOpenGLShaderProgram;
+
+
+CRANBERRY_BEGIN_NAMESPACE
+
+
+///
+/// Contains basic functionality for all
+/// renderable objects within cranberry.
+///
+/// \class Drawable
+/// \author Nicolas Kogler
+/// \date February 11, 2017
+///
+class CRANBERRY_EXPORT Drawable
+{
+public:
+
+    CRANBERRY_DISABLE_MOVE(Drawable)
+
+    ///
+    /// Initializes a new instance of Drawable.
+    ///
+    Drawable();
+
+    ///
+    /// Copies one Drawable instance to another
+    /// while sharing the OpenGL resources.
+    ///
+    /// \param other The other Drawable instance.
+    ///
+    Drawable(const Drawable& other);
+
+    ///
+    /// Copies one Drawable instance to another
+    /// while sharing the OpenGL resources.
+    ///
+    /// \param other The other Drawable instance.
+    ///
+    Drawable& operator=(const Drawable& other);
+
+    ///
+    /// Destroys this Drawable instance.
+    ///
+    virtual ~Drawable();
+
+
+    ///
+    /// Retrieves the current render target.
+    ///
+    /// \returns the associated window.
+    ///
+    Window* renderTarget() const;
+
+    ///
+    /// Retrieves the blending mode.
+    ///
+    /// \returns the blending mode.
+    ///
+    BlendModes blendMode() const;
+
+    ///
+    /// Retrieves the custom shader program
+    /// for this renderable cranberry object.
+    ///
+    /// \returns the custom shader program.
+    ///
+    QOpenGLShaderProgram* customProgram() const;
+
+
+    ///
+    /// Specifies the render target for this object.
+    /// If a nullptr is passed, the active window as
+    /// returned by Window::activeWindow() will be
+    /// used as render target.
+    ///
+    /// \param target The window to render this object on.
+    ///
+    void setRenderTarget(Window* target);
+
+    ///
+    /// Specifies one or more blend modes in which
+    /// the blend color should be rendered on top of
+    /// the object. Multiple blend modes are possible
+    /// by combining the cran::BlendMode flags via OR.
+    ///
+    /// \param mode New blend mode(s).
+    ///
+    void setBlendMode(BlendModes mode);
+
+    ///
+    /// Specifies the custom shader program for this object.
+    /// In order to use the default one, pass a null pointer.
+    /// You do not have to free the memory you allocate by
+    /// creating an instance of the given program.
+    ///
+    /// \param program Custom shader program.
+    /// \remarks The documentation provides a list of in/out
+    ///          and location values your shader has to provide.
+    ///
+    void setCustomProgram(QOpenGLShaderProgram* program);
+
+
+    // Functions to override.
+    virtual void destroy() = 0;
+    virtual void update(const GameTime& time) = 0;
+    virtual void render() = 0;
+
+
+protected:
+
+    ///
+    /// Allocates one vertex buffer object.
+    ///
+    /// \param target Render target to use. Can not be nullptr.
+    /// \param size The size (in bytes) to allocate.
+    /// \returns false if no render target specified.
+    ///
+    bool createInternal(Window* target, int32_t size);
+
+    ///
+    /// Destroys all OpenGL resources.
+    ///
+    void destroyInternal();
+
+    ///
+    /// Returns the private vertex buffer.
+    ///
+    /// \returns the private VBO.
+    ///
+    QOpenGLBuffer* vertexBuffer() const;
+
+
+    // Protected members
+    QOpenGLFunctions* gl;
+
+
+private:
+
+    // Members
+    uint32_t*               m_refCount;
+    Window*                 m_renderTarget;
+    QOpenGLBuffer*          m_vertexBuffer;
+    QOpenGLShaderProgram*   m_customProgram;
+    BlendModes              m_blendModes;
+    bool                    m_isInit;
+};
+
+
+CRANBERRY_END_NAMESPACE
+
+
+#endif  // CRANBERRY_DRAWABLE_HPP
---
+++ src/Graphics/System/Drawable.cpp
@@ -0,0 +1,169 @@
+﻿//
+//  cranberry: C++ game engine using the Qt framework and OpenGL/ES.
+//  Copyright (C) 2017 Nicolas Kogler
+//
+//  Cranberry is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Cranberry is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with cranberry. If not, see <http://www.gnu.org/licenses/>.
+//
+
+
+// Cranberry headers
+#include <Cranberry/Graphics/System/Drawable.hpp>
+
+// Qt headers
+#include <QOpenGLBuffer>
+#include <QOpenGLContext>
+#include <QOpenGLFunctions>
+#include <QOpenGLShaderProgram>
+
+
+CRANBERRY_BEGIN_NAMESPACE
+
+
+Drawable::Drawable()
+    : m_refCount(new uint32_t(1))
+    , m_renderTarget(nullptr)
+    , m_vertexBuffer(nullptr)
+    , m_customProgram(nullptr)
+    , m_blendModes(NoBlend)
+    , m_isInit(false)
+{
+}
+
+
+Drawable::Drawable(const Drawable& other)
+    : m_refCount(other.m_refCount)
+    , m_renderTarget(other.m_renderTarget)
+    , m_vertexBuffer(other.m_vertexBuffer)
+    , m_customProgram(other.m_customProgram)
+    , m_blendModes(other.m_blendModes)
+    , m_isInit(other.m_isInit)
+{
+    *m_refCount += 1;
+}
+
+
+Drawable& Drawable::operator =(const Drawable& other)
+{
+    m_refCount = other.m_refCount;
+    m_renderTarget = other.m_renderTarget;
+    m_vertexBuffer = other.m_vertexBuffer;
+    m_customProgram = other.m_customProgram;
+    m_blendModes = other.m_blendModes;
+    m_isInit = other.m_isInit;
+   *m_refCount += 1;
+
+    return *this;
+}
+
+
+Drawable::~Drawable()
+{
+    *m_refCount -= 1;
+
+    if (*m_refCount == 0 && m_isInit)
+        destroyInternal();
+}
+
+
+Window* Drawable::renderTarget() const
+{
+    return m_renderTarget;
+}
+
+
+BlendModes Drawable::blendMode() const
+{
+    return m_blendModes;
+}
+
+
+QOpenGLShaderProgram* Drawable::customProgram() const
+{
+    return m_customProgram;
+}
+
+
+void Drawable::setRenderTarget(Window* target)
+{
+    // If no target specified, gets active window.
+    if (target == nullptr)
+        target = Window::activeWindow();
+
+    // If already created, change the functions too.
+    if (m_isInit)
+        gl = target->context()->functions();
+
+    m_renderTarget = target;
+}
+
+
+void Drawable::setBlendMode(BlendModes mode)
+{
+    m_blendModes = mode;
+}
+
+
+void Drawable::setCustomProgram(QOpenGLShaderProgram* program)
+{
+    delete m_customProgram;
+    m_customProgram = program;
+}
+
+
+bool Drawable::createInternal(Window* target, int32_t size)
+{
+    // Does not accept invalid target.
+    if (target == nullptr)
+        return false;
+
+    target->makeCurrent();
+    gl = target->context()->functions();
+    m_vertexBuffer = new QOpenGLBuffer(QOpenGLBuffer::VertexBuffer);
+    m_renderTarget = target;
+    m_isInit = true;
+
+    // Attempts to create the vertex buffer.
+    if (!m_vertexBuffer->create())
+        return false;
+
+    m_vertexBuffer->bind();
+    m_vertexBuffer->setUsagePattern(QOpenGLBuffer::DynamicDraw);
+    m_vertexBuffer->allocate(size);
+    return true;
+}
+
+
+void Drawable::destroyInternal()
+{
+    // Does not accept invalid target.
+    if (m_renderTarget == nullptr)
+        return;
+
+    m_renderTarget->makeCurrent();
+    m_vertexBuffer->destroy();
+    m_isInit = false;
+
+    delete m_refCount;
+    delete m_vertexBuffer;
+    delete m_customProgram;
+}
+
+
+QOpenGLBuffer* Drawable::vertexBuffer() const
+{
+    return m_vertexBuffer;
+}
+
+
+CRANBERRY_END_NAMESPACE
