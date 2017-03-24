470d01208d77fa97d808877140f2641484c8d628
Nicolas Kogler <nicolas.kogler@hotmail.com>
2017-03-24 23:30:52 +0100

    First prototype of the new cran::Text class.

---
+++ include/Cranberry/Graphics/Text.hpp
@@ -0,0 +1,175 @@
+﻿//
+//  cranberry: C++ game engine using the Qt framework and OpenGL/ES.
+//  Copyright (C) 2017 Nicolas Kogler
+//
+//  Cranberry is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Cranberry is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with cranberry. If not, see <http://www.gnu.org/licenses/>.
+//
+
+
+#pragma once
+#ifndef CRANBERRY_TEXT_HPP
+#define CRANBERRY_TEXT_HPP
+
+
+// Cranberry headers
+#include <Cranberry/Graphics/System/Drawable.hpp>
+#include <Cranberry/Graphics/System/Fadable.hpp>
+#include <Cranberry/Graphics/System/Transformable.hpp>
+
+// Qt headers
+#include <QBrush>
+#include <QFont>
+#include <QPen>
+
+
+CRANBERRY_BEGIN_NAMESPACE
+
+
+///
+/// Defines a renderable text.
+///
+/// \class Text
+/// \author Nicolas Kogler
+/// \date March 22, 2017
+///
+class CRANBERRY_EXPORT Text
+        : public Drawable
+        , public Fadable
+        , public Transformable
+{
+public:
+
+    CRANBERRY_DISABLE_MOVE(Text)
+    CRANBERRY_DEFAULT_COPY(Text)
+    CRANBERRY_DEFAULT_DTOR(Text)
+
+    ///
+    /// Initializes a new instance of Text.
+    ///
+    Text();
+
+
+    ///
+    /// Retrieves the currently displayed text.
+    ///
+    /// \returns the current text.
+    ///
+    const QString& text() const;
+
+    ///
+    /// Retrieves the currently used font.
+    ///
+    /// \returns the current font.
+    ///
+    const QFont& font() const;
+
+    ///
+    /// Retrieves the text color.
+    ///
+    /// \returns the text color.
+    ///
+    const QColor& textColor() const;
+
+    ///
+    /// Retrieves the outline color.
+    ///
+    /// \returns the outline color.
+    ///
+    QColor outlineColor() const;
+
+    ///
+    /// Retrieves the outline width.
+    ///
+    /// \returns the outline width.
+    ///
+    int32_t outlineWidth() const;
+
+
+    ///
+    /// Specifies the string to be displayed.
+    ///
+    /// \param str String to display.
+    ///
+    void setText(const QString& str);
+
+    ///
+    /// Specifies the font to render the text with.
+    ///
+    /// \param font Font used for rendering.
+    ///
+    void setFont(const QFont& font);
+
+    ///
+    /// Specifies the color of the text.
+    ///
+    /// \param color New text color.
+    ///
+    void setTextColor(const QColor& color);
+
+    ///
+    /// Specifies the color of the outline.
+    ///
+    /// \param color New outline color.
+    ///
+    void setOutlineColor(const QColor& color);
+
+    ///
+    /// Specifies the width of the outline.
+    /// By default, this value is zero.
+    ///
+    /// \param width New outline width.
+    ///
+    void setOutlineWidth(int32_t width);
+
+
+    ///
+    /// Creates the OpenGL resources for this object.
+    ///
+    /// \param renderTarget The target to render text on.
+    ///
+    void create(Window* renderTarget);
+
+    ///
+    /// Destroys the OpenGL resources for this object.
+    ///
+    void destroy() override;
+
+    ///
+    /// Updates the transform and opacity of this object.
+    ///
+    /// \param time Contains the delta time.
+    ///
+    void update(const GameTime& time) override;
+
+    ///
+    /// Renders this object.
+    ///
+    void render() override;
+
+
+private:
+
+    // Members
+    QString m_text;
+    QFont   m_font;
+    QBrush  m_textBrush;
+    QPen    m_outlinePen;
+    int32_t m_outlineSize;
+};
+
+
+CRANBERRY_END_NAMESPACE
+
+
+#endif  // CRANBERRY_TEXT_HPP
---
+++ src/Graphics/Text.cpp
@@ -0,0 +1,147 @@
+﻿//
+//  cranberry: C++ game engine using the Qt framework and OpenGL/ES.
+//  Copyright (C) 2017 Nicolas Kogler
+//
+//  Cranberry is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Cranberry is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with cranberry. If not, see <http://www.gnu.org/licenses/>.
+//
+
+
+// Cranberry headers
+#include <Cranberry/Graphics/Text.hpp>
+
+// Qt headers
+#include <QPainter>
+
+
+CRANBERRY_BEGIN_NAMESPACE
+
+
+Text::Text()
+    : Drawable()
+    , Fadable()
+    , Transformable()
+    , m_text(QUuid::createUuid().toString())
+    , m_textBrush(QBrush(Qt::white))
+    , m_outlinePen(QPen(Qt::black))
+    , m_outlineSize(0)
+{
+    m_outlinePen.setWidth(m_outlineSize);
+}
+
+
+const QString& Text::text() const
+{
+    return m_text;
+}
+
+
+const QFont& Text::font() const
+{
+    return m_font;
+}
+
+
+const QColor& Text::textColor() const
+{
+    return m_textBrush.color();
+}
+
+
+QColor Text::outlineColor() const
+{
+    return m_outlinePen.color();
+}
+
+
+int32_t Text::outlineWidth() const
+{
+    return m_outlineSize;
+}
+
+
+void Text::setText(const QString& str)
+{
+    m_text = str;
+}
+
+
+void Text::setFont(const QFont& font)
+{
+    m_font = font;
+}
+
+void Text::setTextColor(const QColor& color)
+{
+    m_textBrush.setColor(color);
+}
+
+
+void Text::setOutlineColor(const QColor& color)
+{
+    m_outlinePen.setColor(color);
+}
+
+
+void Text::setOutlineWidth(int32_t width)
+{
+    m_outlinePen.setWidth(width);
+}
+
+
+void Text::create(Window* target)
+{
+    createInternal(target, 0);
+}
+
+
+void Text::destroy()
+{
+    destroyInternal();
+}
+
+
+void Text::update(const GameTime& time)
+{
+    updateFade(time);
+    updateTransform(time);
+}
+
+
+void Text::render()
+{
+    // Creates a painter path that contains the text.
+    QPainterPath path;
+    path.addText(QPointF(), m_font, m_text);
+
+    // Creates a painter that draws onto the current render target.
+    QPainter painter(renderTarget());
+    painter.setRenderHints(QPainter::HighQualityAntialiasing |
+                           QPainter::TextAntialiasing |
+                           QPainter::Antialiasing);
+
+    // Transformations
+    painter.translate(x(), y());
+    painter.scale(scale(), scale());
+    painter.rotate(angle());
+    painter.setOpacity(opacity());
+
+    // Appearance
+    painter.setFont(m_font);
+    painter.setPen(QColor(Qt::red));
+    painter.setBrush(m_textBrush);
+    painter.drawPath(path);
+}
+
+
+CRANBERRY_END_NAMESPACE
