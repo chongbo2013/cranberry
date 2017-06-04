e7b526fa890e829fa0a6eff80e5dec8f5bb35633
Nicolas Kogler <nicolas.kogler@hotmail.com>
2017-06-04 17:46:00 +0200

    Add the base class for all graphic objects.

---
+++ code/include/Cranberry/Graphics/Base/IRenderable.hpp
@@ -0,0 +1,215 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+//
+// Cranberry - C++ game engine based on the Qt5 framework.
+// Copyright (C) 2017 Nicolas Kogler
+//
+// Cranberry is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Cranberry is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Cranberry. If not, see <http://www.gnu.org/licenses/>.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+
+#pragma once
+#ifndef CRANBERRY_IRENDERABLE_HPP
+#define CRANBERRY_IRENDERABLE_HPP
+
+
+// Cranberry headers
+#include <Cranberry/System/GameTime.hpp>
+
+// Qt headers
+#include <QObject>
+
+
+// Forward declarations
+CRANBERRY_FORWARD_Q(QOpenGLFunctions)
+CRANBERRY_FORWARD_C(Window)
+CRANBERRY_FORWARD_C(OpenGLShader)
+
+
+CRANBERRY_BEGIN_NAMESPACE
+
+
+////////////////////////////////////////////////////////////////////////////////
+/// Provides an interface for all renderable objects in cranberry.
+///
+/// \class IRenderable
+/// \author Nicolas Kogler
+/// \date June 4, 2017
+///
+////////////////////////////////////////////////////////////////////////////////
+class CRANBERRY_GRAPHICS_EXPORT IRenderable : public QObject
+{
+public:
+
+    CRANBERRY_DISABLE_COPY(IRenderable)
+    CRANBERRY_DISABLE_MOVE(IRenderable)
+
+    ////////////////////////////////////////////////////////////////////////////
+    /// Constructs a new IRenderable instance. Setting a name is not required,
+    /// but recommended, since it can be easily retrieved by name and it
+    /// simplifies debugging by being able to track down the error source.
+    ///
+    /// \param (opt) name Name of this object.
+    /// \param (opt) parent Parent of this object.
+    ///
+    ////////////////////////////////////////////////////////////////////////////
+    IRenderable(QString name = "", QObject* parent = nullptr);
+
+    ////////////////////////////////////////////////////////////////////////////
+    /// Destructs this IRenderable instance and calls IRenderable::destroy() as
+    /// last resort to free OpenGL-related objects.
+    ///
+    ////////////////////////////////////////////////////////////////////////////
+    virtual ~IRenderable();
+
+
+    ////////////////////////////////////////////////////////////////////////////
+    /// Determines whether this IRenderable object is valid. If a devired class
+    /// needs more conditions for the object to be valid, this method should
+    /// be overridden.
+    ///
+    /// \returns true if this object is not valid.
+    ///
+    ////////////////////////////////////////////////////////////////////////////
+    virtual bool isNull() const;
+
+    ////////////////////////////////////////////////////////////////////////////
+    /// Creates this object by specifying the render target and retrieving the
+    /// OpenGL functions from its context.
+    ///
+    /// \param renderTarget Window to render object on.
+    /// \returns false if initialization failed.
+    ///
+    ////////////////////////////////////////////////////////////////////////////
+    virtual bool create(Window* renderTarget);
+
+    ////////////////////////////////////////////////////////////////////////////
+    /// Destroys the custom shader object, if any. It is recommended to
+    /// override this method in order to implement custom clean-up.
+    ///
+    ////////////////////////////////////////////////////////////////////////////
+    virtual void destroy();
+
+    ////////////////////////////////////////////////////////////////////////////
+    /// Updates the object. Must implement.
+    ///
+    ////////////////////////////////////////////////////////////////////////////
+    virtual void update(const GameTime& time) = 0;
+
+    ////////////////////////////////////////////////////////////////////////////
+    /// Renders the object. Must implement.
+    ///
+    ////////////////////////////////////////////////////////////////////////////
+    virtual void render() = 0;
+
+
+    ////////////////////////////////////////////////////////////////////////////
+    /// Retrieves a pointer to the shader program. If no custom shader program
+    /// was specified using IRenderable::setShaderProgram, the default shader
+    /// program for this object will be returned.
+    ///
+    /// \returns a pointer to the currently used shader program.
+    ///
+    ////////////////////////////////////////////////////////////////////////////
+    OpenGLShader* shaderProgram() const;
+
+    ////////////////////////////////////////////////////////////////////////////
+    /// Specifies the shader program. If the given program is nullptr, the
+    /// default shader program will be used instead.
+    ///
+    /// \param program Program to use. Nullptr yields default shader program.
+    ///
+    ////////////////////////////////////////////////////////////////////////////
+    void setShaderProgram(OpenGLShader* program);
+
+
+signals:
+
+    ////////////////////////////////////////////////////////////////////////////
+    /// Calls the connected slot as soon as this IRenderable object was created,
+    /// i.e. when a call to IRenderable::create() was successful.
+    ///
+    ////////////////////////////////////////////////////////////////////////////
+    void created();
+
+    ////////////////////////////////////////////////////////////////////////////
+    /// Calls the connected slot as soon as this IRenderable object was
+    /// destroyed, i.e. when a call to IRenderable::destroy() was performed.
+    ///
+    ////////////////////////////////////////////////////////////////////////////
+    void destroyed();
+
+
+protected:
+
+    ////////////////////////////////////////////////////////////////////////////
+    /// Specifies the default shader program. Only available to sub-classes.
+    ///
+    /// \param program Program to use. Must not be nullptr.
+    ///
+    ////////////////////////////////////////////////////////////////////////////
+    void setDefaultShaderProgram(OpenGLShader* program);
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Protected members
+    ////////////////////////////////////////////////////////////////////////////
+    QOpenGLFunctions* gl;
+
+
+private:
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Members
+    ////////////////////////////////////////////////////////////////////////////
+    Window*         m_renderTarget;   ///< Target to render object on
+    OpenGLShader*   m_defaultProgram; ///< Default shader program
+    OpenGLShader*   m_customProgram;  ///< Custom shader program
+
+    Q_OBJECT
+};
+
+
+////////////////////////////////////////////////////////////////////////////////
+/// \class IRenderable
+/// \ingroup Graphics
+///
+/// The IRenderable class is the base for all graphic objects in cranberry. It
+/// provides the render target, the shader program and the OpenGL functions.
+///
+/// \code
+/// class MyObject : public IRenderable
+/// {
+/// public:
+///     bool isNull() const override;
+///     bool create() override;
+///     void destroy() override;
+///     void update(const GameTime& time) override;
+///     void render() override;
+///     ...
+/// };
+///
+/// {
+///     renderTarget()->makeCurrent();
+///     ...
+///     gl->glDrawArrays(...);
+/// }
+/// \endcode
+///
+////////////////////////////////////////////////////////////////////////////////
+
+
+CRANBERRY_END_NAMESPACE
+
+
+#endif
---
+++ code/src/Graphics/Base/IRenderable.cpp
@@ -0,0 +1,114 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+//
+// Cranberry - C++ game engine based on the Qt5 framework.
+// Copyright (C) 2017 Nicolas Kogler
+//
+// Cranberry is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Cranberry is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Cranberry. If not, see <http://www.gnu.org/licenses/>.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+
+// Cranberry headers
+#include <Cranberry/Graphics/Base/IRenderable.hpp>
+#include <Cranberry/OpenGL/OpenGLShader.hpp>
+#include <Cranberry/System/Debug.hpp>
+
+// Qt headers
+#include <QOpenGLFunctions>
+
+
+CRANBERRY_USING_NAMESPACE
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Constants
+////////////////////////////////////////////////////////////////////////////////
+namespace
+{
+const QString e_01("%0 [%1] - The given render target is invalid.");
+const QString e_02("%0 [%1] - There is no default shader program.");
+}
+
+
+IRenderable::IRenderable(QString name, QObject* parent)
+    : QObject(parent)
+    , gl(nullptr)
+    , m_renderTarget(nullptr)
+    , m_defaultProgram(nullptr)
+    , m_customProgram(nullptr)
+{
+    setObjectName(name);
+}
+
+
+IRenderable::~IRenderable()
+{
+    destroy();
+}
+
+
+bool IRenderable::isNull() const
+{
+    return m_renderTarget == nullptr;
+}
+
+
+bool IRenderable::create(Window* renderTarget)
+{
+    if (renderTarget == nullptr)
+    {
+        // TODO: Uncomment as soon as Window was coded.
+        //if ((renderTarget = Window::activeWindow()) == nullptr)
+        //{
+        //    return cranError(e_01.arg(CRANBERRY_FUNC, objectName()));
+        //}
+    }
+
+    // TODO: Uncomment as soon as Window was coded.
+    //gl = renderTarget->context()->functions();
+    m_renderTarget = renderTarget;
+
+    emit created();
+    return true;
+}
+
+
+void IRenderable::destroy()
+{
+    delete m_customProgram;
+}
+
+
+OpenGLShader* IRenderable::shaderProgram() const
+{
+    if (Q_UNLIKELY(m_defaultProgram == nullptr))
+    {
+        cranError(e_02.arg(CRANBERRY_FUNC, objectName()));
+        return nullptr;
+    }
+
+    return (m_customProgram != nullptr) ? m_customProgram : m_defaultProgram;
+}
+
+
+void IRenderable::setShaderProgram(OpenGLShader* program)
+{
+    m_customProgram = program;
+}
+
+
+void IRenderable::setDefaultShaderProgram(OpenGLShader* program)
+{
+    m_defaultProgram = program;
+}
