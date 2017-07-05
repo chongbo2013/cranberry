48cb1b27572475cac81c53d37a19a7dd3e95f035
Nicolas Kogler <nicolas.kogler@hotmail.com>
2017-07-05 17:52:34 +0200

    Add a new base class for all shapes.

---
+++ code/include/Cranberry/Graphics/Base/IShape.hpp
@@ -0,0 +1,215 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+//
+// Cranberry - C++ game engine based on the Qt5 framework.
+// Copyright (C) 2017 Nicolas Kogler
+//
+// Cranberry is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Cranberry is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Cranberry. If not, see <http://www.gnu.org/licenses/>.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+
+#pragma once
+#ifndef CRANBERRY_ISHAPE_HPP
+#define CRANBERRY_ISHAPE_HPP
+
+
+// Cranberry headers
+#include <Cranberry/Graphics/Base/Enumerations.hpp>
+#include <Cranberry/Graphics/Base/IRenderable.hpp>
+#include <Cranberry/Graphics/Base/ITransformable.hpp>
+#include <Cranberry/OpenGL/OpenGLVertex.hpp>
+
+// Qt headers
+#include <QMatrix4x4>
+
+
+// Forward declarations
+CRANBERRY_FORWARD_Q(QOpenGLBuffer)
+
+
+CRANBERRY_BEGIN_NAMESPACE
+
+
+////////////////////////////////////////////////////////////////////////////////
+/// Defines the base class for rendering primitives.
+///
+/// \class IShape
+/// \author Nicolas Kogler
+/// \date July 5, 2017
+///
+////////////////////////////////////////////////////////////////////////////////
+class CRANBERRY_GRAPHICS_EXPORT IShape
+        : public IRenderable
+        , public ITransformable
+{
+public:
+
+    CRANBERRY_DISABLE_COPY(IShape)
+    CRANBERRY_DISABLE_MOVE(IShape)
+
+    ////////////////////////////////////////////////////////////////////////////
+    /// Initializes a new instance of the ITexture class and sets all members
+    /// to their logical default values.
+    ///
+    ////////////////////////////////////////////////////////////////////////////
+    IShape();
+
+    ////////////////////////////////////////////////////////////////////////////
+    /// Destroys this texture. Last resort for releasing all OpenGL resources
+    /// before the context will eventually be destroyed.
+    ///
+    ////////////////////////////////////////////////////////////////////////////
+    virtual ~IShape();
+
+
+    ////////////////////////////////////////////////////////////////////////////
+    /// Reimplements IRenderable::isNull(). Adds the condition that the
+    /// vertex buffer is valid.
+    ///
+    /// \returns true if the object is invalid.
+    ///
+    ////////////////////////////////////////////////////////////////////////////
+    virtual bool isNull() const override;
+
+    ////////////////////////////////////////////////////////////////////////////
+    /// Destroys all OpenGL resources allocated for this object.
+    ///
+    ////////////////////////////////////////////////////////////////////////////
+    virtual void destroy() override;
+
+    ////////////////////////////////////////////////////////////////////////////
+    /// Updates the transformations of this object.
+    ///
+    /// \param time Contains the delta time.
+    ///
+    ////////////////////////////////////////////////////////////////////////////
+    virtual void update(const GameTime& time) override;
+
+    ////////////////////////////////////////////////////////////////////////////
+    /// Renders this object.
+    ///
+    ////////////////////////////////////////////////////////////////////////////
+    virtual void render() override;
+
+
+    ////////////////////////////////////////////////////////////////////////////
+    /// Retrieves the amount of vertices for this object.
+    ///
+    /// \returns the amount of vertices.
+    ///
+    ////////////////////////////////////////////////////////////////////////////
+    uint vertexCount() const;
+
+    ////////////////////////////////////////////////////////////////////////////
+    /// Determines whether this shape is filled.
+    ///
+    /// \returns true if filled.
+    ///
+    ////////////////////////////////////////////////////////////////////////////
+    bool isShapeFilled() const;
+
+
+    ////////////////////////////////////////////////////////////////////////////
+    /// Renders this primitive filled or wired.
+    ///
+    /// \param filled True to fill primitive.
+    ///
+    ////////////////////////////////////////////////////////////////////////////
+    void setShapeFilled(bool filled);
+
+    ////////////////////////////////////////////////////////////////////////////
+    /// Specifies the color that will be applied on this object.
+    ///
+    /// \param color Color to use for blending.
+    ///
+    ////////////////////////////////////////////////////////////////////////////
+    void setColor(const QColor& color);
+
+    ////////////////////////////////////////////////////////////////////////////
+    /// Specifies the colors that will be applied on this object. Note that the
+    /// amount of
+    ///
+    /// \param tl Top left vertex.
+    /// \param tr Top right vertex.
+    /// \param br Bottom right vertex.
+    /// \param bl Bottom left vertex.
+    ///
+    ////////////////////////////////////////////////////////////////////////////
+    void setColor(const QVector<QColor>& colors);
+
+
+protected:
+
+    virtual uint renderModeWired() const = 0;
+    virtual uint renderModeFilled() const = 0;
+    bool createInternal(const QVector<QVector2D>& points, Window* renderTarget);
+
+
+private:
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Helpers
+    ////////////////////////////////////////////////////////////////////////////
+    bool createBuffer();
+    auto findCenter(const QVector<QVector2D>&) -> QVector2D;
+    auto findSize(const QVector<QVector2D>&) -> QVector2D;
+    auto buildMatrix() -> QMatrix4x4;
+    void bindObjects();
+    void releaseObjects();
+    void writeVertices();
+    void modifyProgram();
+    void modifyAttribs();
+    void drawElements();
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Members
+    ////////////////////////////////////////////////////////////////////////////
+    priv::VarVertices   m_vertices;
+    QOpenGLBuffer*      m_vertexBuffer;
+    QVector<QColor>     m_colorBuffer;
+    bool                m_filled;
+    bool                m_colorUpdate;
+    bool                m_update;
+};
+
+
+////////////////////////////////////////////////////////////////////////////////
+/// \class IShape
+/// \ingroup Graphics
+///
+/// This class is the base for all shape objects. The renderModeWired() and the
+/// renderModeFilled() expect values like GL_LINES, GL_TRIANGLES and so on.
+///
+/// \code
+/// class Polygon : public IShape
+/// {
+/// public:
+///
+///     bool create(...);
+///
+///
+/// protected:
+///
+///    uint renderModeWired() const override;
+///    uint renderModeFilled() const override;
+/// };
+/// \endcode
+///
+////////////////////////////////////////////////////////////////////////////////
+
+
+CRANBERRY_END_NAMESPACE
+
+
+#endif
---
+++ code/src/Graphics/Base/IShape.cpp
@@ -0,0 +1,353 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+//
+// Cranberry - C++ game engine based on the Qt5 framework.
+// Copyright (C) 2017 Nicolas Kogler
+//
+// Cranberry is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Cranberry is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Cranberry. If not, see <http://www.gnu.org/licenses/>.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+
+// Cranberry headers
+#include <Cranberry/Graphics/Base/IShape.hpp>
+#include <Cranberry/OpenGL/OpenGLDebug.hpp>
+#include <Cranberry/OpenGL/OpenGLDefaultShaders.hpp>
+#include <Cranberry/OpenGL/OpenGLShader.hpp>
+#include <Cranberry/System/Debug.hpp>
+#include <Cranberry/Window/Window.hpp>
+
+// Qt headers
+#include <QOpenGLBuffer>
+#include <QOpenGLFunctions>
+#include <QOpenGLShaderProgram>
+
+
+CRANBERRY_USING_NAMESPACE
+
+
+CRANBERRY_CONST_VAR(QString, e_01, "%0 [%1] - Vertex buffer creation failed.")
+CRANBERRY_CONST_VAR(QString, e_02, "%0 [%1] - Cannot render invalid object.")
+CRANBERRY_CONST_VAR(QString, e_03, "%0 [%1] - Color count does not match vertex count.")
+
+
+IShape::IShape()
+    : m_vertexBuffer(nullptr)
+    , m_filled(false)
+    , m_colorUpdate(false)
+    , m_update(false)
+{
+}
+
+
+IShape::~IShape()
+{
+    destroy();
+}
+
+
+bool IShape::isNull() const
+{
+    return IRenderable::isNull()     ||
+           m_vertexBuffer == nullptr ||
+          !m_vertexBuffer->isCreated();
+}
+
+
+void IShape::destroy()
+{
+    if (m_vertexBuffer != nullptr)
+    {
+        m_vertexBuffer->destroy();
+        delete m_vertexBuffer;
+        m_vertexBuffer = nullptr;
+    }
+
+    m_colorBuffer.clear();
+    m_update = false;
+
+    IRenderable::destroy();
+}
+
+
+void IShape::update(const GameTime& time)
+{
+    updateTransform(time);
+}
+
+
+void IShape::render()
+{
+    if (Q_UNLIKELY(isNull()))
+    {
+        cranError(ERRARG(e_02));
+        return;
+    }
+    else if (QOpenGLContext::currentContext() != renderTarget()->context())
+    {
+        renderTarget()->makeCurrent();
+    }
+
+    bindObjects();
+    writeVertices();
+    modifyProgram();
+    modifyAttribs();
+    drawElements();
+    releaseObjects();
+}
+
+
+uint IShape::vertexCount() const
+{
+    return m_vertices.size();
+}
+
+
+bool IShape::isShapeFilled() const
+{
+    return m_filled;
+}
+
+
+void IShape::setShapeFilled(bool filled)
+{
+    m_filled = filled;
+}
+
+
+void IShape::setColor(const QColor& color)
+{
+    m_colorBuffer.clear();
+    m_colorBuffer.append(color);
+    m_colorUpdate = true;
+    m_update = true;
+}
+
+
+void IShape::setColor(const QVector<QColor>& colors)
+{
+    if (colors.size() != (int) m_vertices.size())
+    {
+        cranError(ERRARG(e_03));
+        return;
+    }
+
+    m_colorBuffer = colors;
+    m_colorUpdate = true;
+    m_update = true;
+}
+
+
+bool IShape::createInternal(const QVector<QVector2D>& points, Window* rt)
+{
+    if (!IRenderable::create(rt)) return false;
+    if (!createBuffer()) return false;
+
+    // Allocates as much data as we need to store all points.
+    m_vertexBuffer->allocate(priv::Vertex::size() * points.size());
+    m_vertexBuffer->release();
+    m_vertices.clear();
+    m_update = true;
+
+    auto size = findSize(points);
+    for (const QVector2D& p : points)
+    {
+        priv::Vertex v;
+        v.xyz(p.x(), p.y(), 0);
+        m_vertices.push_back(v);
+    }
+
+    setOrigin(findCenter(points));
+    setDefaultShaderProgram(OpenGLDefaultShaders::get("cb.glsl.shape"));
+    setSize(size.x(), size.y());
+
+    return true;
+}
+
+
+bool IShape::createBuffer()
+{
+    // Attempts to create the buffer holding the vertex data.
+    m_vertexBuffer = new QOpenGLBuffer(QOpenGLBuffer::VertexBuffer);
+    if (!m_vertexBuffer->create() || !m_vertexBuffer->bind())
+    {
+        return cranError(ERRARG(e_01));
+    }
+
+    m_vertexBuffer->setUsagePattern(QOpenGLBuffer::DynamicDraw);
+
+    return true;
+}
+
+
+QVector2D IShape::findCenter(const QVector<QVector2D>& points)
+{
+    QVector2D center;
+    qreal signedArea = 0.0, x0 = 0.0, y0 = 0.0, x1 = 0.0, y1 = 0.0, a = 0.0;
+
+    // Computes the centroid for each vertex except the last.
+    int size = points.size() - 1, i;
+    for (i = 0; i < size; i++)
+    {
+        x0 = points.at(i).x();
+        y0 = points.at(i).y();
+        x1 = points.at(i+1).x();
+        y1 = points.at(i+1).y();
+        a = x0 * y1 - x1 * y0;
+
+        signedArea += a;
+        center.setX(center.x() + (x0 + x1) * a);
+        center.setY(center.y() + (y0 + y1) * a);
+    }
+
+    // Compute last vertex separately.
+    x0 = points.at(i).x();
+    y0 = points.at(i).y();
+    x1 = points.at(0).x();
+    y1 = points.at(0).y();
+    a = x0 * y1 - x1 * y0;
+
+    signedArea += a;
+    center.setX(center.x() + (x0 + x1) * a);
+    center.setY(center.y() + (y0 + y1) * a);
+
+    signedArea *= 0.5;
+    center.setX(center.x() / (signedArea * 6.0));
+    center.setY(center.y() / (signedArea * 6.0));
+
+    return center;
+}
+
+
+QVector2D IShape::findSize(const QVector<QVector2D>& points)
+{
+    auto extremeX = std::minmax_element(
+                points.begin(), points.end(),
+                [] (const QVector2D& l, const QVector2D& r) {
+                    return l.x() < r.x();
+                });
+
+    auto extremeY = std::minmax_element(
+                points.begin(), points.end(),
+                [] (const QVector2D& l, const QVector2D& r) {
+                    return l.y() < r.y();
+                });
+
+    QVector2D upperLeft(extremeX.first->x(), extremeY.first->y());
+    QVector2D lowerRight(extremeX.second->x(), extremeY.second->y());
+
+    return lowerRight - upperLeft;
+}
+
+
+QMatrix4x4 IShape::buildMatrix()
+{
+    float fw = static_cast<float>(renderTarget()->width());
+    float fh = static_cast<float>(renderTarget()->height());
+
+    // TODO: Not cheap - maybe optimize this?!
+    QMatrix4x4 proj, tran, rot, scale, orig, norig;
+    proj.ortho(0.f, fw, fh, 0.f, -1.f, 1.f);
+    tran.translate(x(), y());
+    rot.rotate(angleX(), 1.f, 0.f, 0.f);
+    rot.rotate(angleY(), 0.f, 1.f, 0.f);
+    rot.rotate(angleZ(), 0.f, 0.f, 1.f);
+    scale.scale(scaleX(), scaleY());
+    orig.translate(origin());
+    norig.translate(origin() * -1);
+
+    return proj * tran * orig * rot * norig * orig * scale * norig;
+}
+
+
+void IShape::bindObjects()
+{
+    glDebug(m_vertexBuffer->bind());
+    glDebug(shaderProgram()->program()->bind());
+}
+
+
+void IShape::releaseObjects()
+{
+    glDebug(m_vertexBuffer->release());
+    glDebug(shaderProgram()->program()->release());
+}
+
+
+void IShape::writeVertices()
+{
+    if (m_update || m_colorUpdate)
+    {
+        if (m_colorUpdate)
+        {
+            bool singleColor = m_colorBuffer.size() == 1;
+            int size = (int) vertexCount();
+
+            // Colors every vertex.
+            for (int i = 0; i < size; i++)
+            {
+                if (singleColor) m_vertices[i].rgba(m_colorBuffer.at(0));
+                else m_vertices[i].rgba(m_colorBuffer.at(i));
+            }
+        }
+
+        // Uploads the vertex data.
+        glDebug(m_vertexBuffer->write(
+                    0, m_vertices.data(),
+                    priv::Vertex::size() * vertexCount())
+                    );
+
+        m_update = false;
+        m_colorUpdate = false;
+    }
+}
+
+
+void IShape::modifyProgram()
+{
+    QOpenGLShaderProgram* program = shaderProgram()->program();
+
+    glDebug(program->setUniformValue("u_mvp", buildMatrix()));
+    glDebug(program->setUniformValue("u_opac", opacity()));
+    glDebug(program->enableAttributeArray(priv::Vertex::xyzAttrib()));
+    glDebug(program->enableAttributeArray(priv::Vertex::rgbaAttrib()));
+}
+
+
+void IShape::modifyAttribs()
+{
+    glDebug(gl->glVertexAttribPointer(
+                priv::Vertex::xyzAttrib(),
+                priv::Vertex::xyzLength(),
+                GL_FLOAT,
+                GL_FALSE,
+                priv::Vertex::size(),
+                priv::Vertex::xyzOffset()
+                ));
+
+    glDebug(gl->glVertexAttribPointer(
+                priv::Vertex::rgbaAttrib(),
+                priv::Vertex::rgbaLength(),
+                GL_FLOAT,
+                GL_FALSE,
+                priv::Vertex::size(),
+                priv::Vertex::rgbaOffset()
+                ));
+}
+
+
+void IShape::drawElements()
+{
+    uint mode = (m_filled) ? renderModeFilled() : renderModeWired();
+    glDebug(gl->glDrawArrays(mode, GL_ZERO, vertexCount()));
+}
