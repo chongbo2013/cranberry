a881f143d948ef47e2fc661d7f73557150fa5204
Nicolas Kogler <nicolas.kogler@hotmail.com>
2017-07-14 03:45:45 +0200

    Game: Force DPI to be 96.

--- code/src/Game/Game.cpp
+++ code/src/Game/Game.cpp
@@ -60,6 +60,7 @@ Game::Game(int& argc, char* argv[])
     // Creates the GUI application, if not already.
     if (g_application == nullptr)
     {
+        QApplication::setAttribute(Qt::AA_Use96Dpi);
         g_application = new QApplication(argc, argv);
     }
 
