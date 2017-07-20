e2a067c1b05d08b04967daf0e2de92015c33a74e
Nicolas Kogler <nicolas.kogler@hotmail.com>
2017-07-20 14:31:29 +0200

    Shaders: Fix lens shader.

--- code/resources/glsl/lens_frag.glsl
+++ code/resources/glsl/lens_frag.glsl
@@ -35,8 +35,22 @@ uniform int u_mode; // unused
 uniform int u_effect; // unused
 
 // Lens uniform variables
-uniform float u_radiusX; // Ellipse horizontal radius
-uniform float u_radiusY; // Ellipse vertical radius
+uniform float u_radiusX; // Start of the black area
+uniform float u_radiusY; // End of the black area
+uniform vec4 u_color;    // Lens surrounding color.
+
+// ------------------
+// Parameter meanings
+// u_radiusX (range: 0 - 1)
+// ---------
+// If smaller, the visible area is smaller.
+//
+// u_radiusY (range: 0 - 1)
+// ---------
+// 1. If bigger than u_radiusX, lens is bent to the outside.
+// 2. If smaller than u_radiusX, lens is bent to the inside.
+// 3. If distance to u_radiusX is bigger, gradient is bigger.
+// ------------------
 
 
 void main()
@@ -44,13 +58,13 @@ void main()
     float dist = distance(o_uv, vec2(0.5, 0.5));
     float sstep = smoothstep(u_radiusX, u_radiusY, dist);
 
-    vec4 src = vec4(0.0, 0.0, 0.0, (1 - sstep));
+    vec4 src = vec4(u_color.rgb, u_color.a * (1.0 - sstep));
     vec4 dst = texture(u_tex, o_uv);
 
-    float r = (src.a * src.r + dst.a * (1 - src.a) * dst.r);
-    float g = (src.a * src.g + dst.a * (1 - src.a) * dst.g);
-    float b = (src.a * src.b + dst.a * (1 - src.a) * dst.b);
-    float a = (src.a + dst.a * (1 - src.a));
+    float r = (src.a * src.r + dst.a * (1.0 - src.a) * dst.r);
+    float g = (src.a * src.g + dst.a * (1.0 - src.a) * dst.g);
+    float b = (src.a * src.b + dst.a * (1.0 - src.a) * dst.b);
+    float a = (src.a + dst.a * (1.0 - src.a));
 
     o_pixel = vec4(r, g, b, a);
 }
