16bf6e6295df2ac5eaec71e73e6a4dfa498fef36
Nicolas Kogler <nicolas.kogler@hotmail.com>
2017-08-16 16:23:26 +0200

    Add a tilemap class to draw multiple tiles.
    Optimizes rendering by precomputing all positions and coordinates and
    storing them in a VBO, rendering all vertices in a single glDrawArrays
    call.

---
+++ code/include/Cranberry/Graphics/Tilemap.hpp
@@ -0,0 +1,188 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+//
+// Cranberry - C++ game engine based on the Qt 5.8 framework.
+// Copyright (C) 2017 Nicolas Kogler
+//
+// Cranberry is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Cranberry is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Cranberry. If not, see <http://www.gnu.org/licenses/>.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+
+#pragma once
+#ifndef CRANBERRY_TILEMAP_HPP
+#define CRANBERRY_TILEMAP_HPP
+
+
+// Cranberry headers
+#include <Cranberry/Graphics/Base/RenderBase.hpp>
+#include <Cranberry/OpenGL/OpenGLVertex.hpp>
+
+// Forward declarations
+CRANBERRY_FORWARD_Q(QOpenGLTexture)
+CRANBERRY_FORWARD_Q(QOpenGLBuffer)
+
+
+CRANBERRY_BEGIN_NAMESPACE
+
+
+////////////////////////////////////////////////////////////////////////////////
+/// Renders multiple tiles from a tilemap with a great performance.
+///
+/// \class Tilemap
+/// \author Nicolas Kogler
+/// \date August 16, 2017
+///
+////////////////////////////////////////////////////////////////////////////////
+class CRANBERRY_GRAPHICS_EXPORT Tilemap final : public RenderBase
+{
+public:
+
+    CRANBERRY_DEFAULT_COPY(Tilemap)
+    CRANBERRY_DEFAULT_MOVE(Tilemap)
+
+    Tilemap();
+   ~Tilemap();
+
+
+    ////////////////////////////////////////////////////////////////////////////
+    /// Determines whether this object is null.
+    ///
+    /// \returns true if this batch is null.
+    ///
+    ////////////////////////////////////////////////////////////////////////////
+    bool isNull() const override;
+
+    ////////////////////////////////////////////////////////////////////////////
+    /// Creates the tilemap object.
+    ///
+    /// \param tileset Tileset image to use.
+    /// \param tileSize Size of each tile, in pixels.
+    /// \param mapSize Size of the entire map, in tiles.
+    /// \param view The map view inside the window. A null rectangle leads to
+    ///        having the entire window as view.
+    /// \param renderTarget Target to render batch on.
+    /// \returns true if created successfully.
+    ///
+    ////////////////////////////////////////////////////////////////////////////
+    bool create(
+            const QString& tileset,
+            const QSize& tileSize,
+            const QSize& mapSize,
+            const QRect& view,
+            Window* renderTarget = nullptr
+            );
+
+    ////////////////////////////////////////////////////////////////////////////
+    /// Destroys all the OpenGL objects.
+    ///
+    ////////////////////////////////////////////////////////////////////////////
+    void destroy() override;
+
+    ////////////////////////////////////////////////////////////////////////////
+    /// Updates the tilemap.
+    ///
+    /// \param time Contains the delta time.
+    ///
+    ////////////////////////////////////////////////////////////////////////////
+    void update(const GameTime& time) override;
+
+    ////////////////////////////////////////////////////////////////////////////
+    /// Renders all tiles of the tilemap.
+    ///
+    ////////////////////////////////////////////////////////////////////////////
+    void render() override;
+
+    ////////////////////////////////////////////////////////////////////////////
+    /// Specifies the tiles in the map.
+    ///
+    /// \param tiles List of tile indices.
+    /// \returns false if there are more tiles than the map can actually hold.
+    ///
+    ////////////////////////////////////////////////////////////////////////////
+    bool setTiles(const QVector<uint>& tiles);
+
+    ////////////////////////////////////////////////////////////////////////////
+    /// Appends one single tile.
+    ///
+    /// \param tileIndex The index of the tile within the tileset.
+    /// \returns false if out of bounds.
+    ///
+    ////////////////////////////////////////////////////////////////////////////
+    bool appendTile(uint tileIndex);
+
+    ////////////////////////////////////////////////////////////////////////////
+    /// Removes all tiles from the map.
+    ///
+    ////////////////////////////////////////////////////////////////////////////
+    void removeAllTiles();
+
+
+private:
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Members
+    ////////////////////////////////////////////////////////////////////////////
+    void bindObjects();
+    void releaseObjects();
+    void writeVertices();
+    void modifyProgram();
+    void modifyAttribs();
+    void drawElements();
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Members
+    ////////////////////////////////////////////////////////////////////////////
+    QOpenGLTexture*   m_texture;
+    QOpenGLBuffer*    m_vertexBuffer;
+    priv::MapVertices m_vertices;
+    QRect             m_view;
+    uint              m_tileWidth;
+    uint              m_tileHeight;
+    uint              m_setWidth;
+    uint              m_setHeight;
+    uint              m_mapWidth;
+    uint              m_mapHeight;
+    uint              m_currentX;
+    uint              m_currentY;
+    bool              m_update;
+
+};
+
+
+////////////////////////////////////////////////////////////////////////////////
+/// \class Tilemap
+/// \ingroup Graphics
+///
+/// A tilemap can be used to draw multiple tiles from a tileset in a performant
+/// way.
+///
+/// \code
+/// m_tilemap = new Tilemap;
+/// m_tilemap->create(":/tilesets/set.png", { 32, 32 }, { 40, 40, }, { });
+/// m_tilemap->appendTile(0);
+/// m_tilemap->appendTile(0);
+/// m_tilemap->appendTile(1);
+///
+/// ...
+///
+/// m_tilemap->render();
+/// \endcode
+///
+////////////////////////////////////////////////////////////////////////////////
+
+
+CRANBERRY_END_NAMESPACE
+
+
+#endif
---
+++ code/src/Graphics/Tilemap.cpp
@@ -0,0 +1,302 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+//
+// Cranberry - C++ game engine based on the Qt 5.8 framework.
+// Copyright (C) 2017 Nicolas Kogler
+//
+// Cranberry is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Cranberry is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Cranberry. If not, see <http://www.gnu.org/licenses/>.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+
+// Cranberry headers
+#include <Cranberry/Graphics/Tilemap.hpp>
+#include <Cranberry/OpenGL/OpenGLDebug.hpp>
+#include <Cranberry/OpenGL/OpenGLDefaultShaders.hpp>
+#include <Cranberry/OpenGL/OpenGLShader.hpp>
+#include <Cranberry/System/Debug.hpp>
+#include <Cranberry/Window/Window.hpp>
+
+// Qt headers
+#include <QOpenGLFunctions>
+#include <QOpenGLTexture>
+#include <QOpenGLBuffer>
+
+
+CRANBERRY_USING_NAMESPACE
+
+
+CRANBERRY_CONST_VAR(QString, e_01, "%0 [%1] - Texture could not be created.")
+CRANBERRY_CONST_VAR(QString, e_02, "%0 [%1] - Vertex buffer could not be created.")
+
+
+Tilemap::Tilemap()
+    : m_texture(nullptr)
+    , m_tileWidth(0)
+    , m_tileHeight(0)
+    , m_setWidth(0)
+    , m_setHeight(0)
+    , m_mapWidth(0)
+    , m_mapHeight(0)
+    , m_currentX(0)
+    , m_currentY(0)
+    , m_update(false)
+{
+}
+
+
+Tilemap::~Tilemap()
+{
+    destroy();
+}
+
+
+bool Tilemap::isNull() const
+{
+    return RenderBase::isNull() ||
+           m_vertices.empty()   ||
+           m_texture == nullptr ||
+          !m_texture->isCreated();
+}
+
+
+bool Tilemap::create(
+    const QString& tileset,
+    const QSize& tileSize,
+    const QSize& mapSize,
+    const QRect& view,
+    Window* rt
+    )
+{
+    if (!RenderBase::create(rt)) return false;
+
+    // Attempts to create the texture.
+    m_texture = new QOpenGLTexture(QImage(tileset));
+    if (!m_texture->create())
+    {
+        return cranError(ERRARG(e_01));
+    }
+
+    // Attempts to create the vertex buffer.
+    m_vertexBuffer = new QOpenGLBuffer(QOpenGLBuffer::VertexBuffer);
+    if (!m_vertexBuffer->create() || !m_vertexBuffer->bind())
+    {
+        return cranError(ERRARG(e_02));
+    }
+
+    // Specifies all members.
+    m_tileWidth  = static_cast<uint>(tileSize.width());
+    m_tileHeight = static_cast<uint>(tileSize.height());
+    m_setWidth   = static_cast<uint>(m_texture->width()  / m_tileWidth);
+    m_setHeight  = static_cast<uint>(m_texture->height() / m_tileHeight);
+    m_mapWidth   = static_cast<uint>(mapSize.width());
+    m_mapHeight  = static_cast<uint>(mapSize.height());
+    m_view = view;
+
+    // Allocates the correct amount of vertices for the tiles.
+    m_vertexBuffer->allocate(m_mapWidth * m_mapHeight * 6 * priv::MapVertex::size());
+
+    setDefaultShaderProgram(OpenGLDefaultShaders::get("cb.glsl.texture"));
+    setSize(m_mapWidth * m_tileWidth, m_mapHeight * m_tileHeight);
+    setOrigin(width() / 2, height() / 2);
+
+    return true;
+}
+
+
+void Tilemap::destroy()
+{
+    delete m_texture;
+    delete m_vertexBuffer;
+
+    m_texture = nullptr;
+    m_vertexBuffer = nullptr;
+
+    RenderBase::destroy();
+}
+
+
+void Tilemap::update(const GameTime& time)
+{
+    updateTransform(time);
+}
+
+
+void Tilemap::render()
+{
+    if (!prepareRendering())
+    {
+        return;
+    }
+
+    bindObjects();
+    writeVertices();
+    modifyProgram();
+    modifyAttribs();
+    drawElements();
+    releaseObjects();
+}
+
+
+void Tilemap::bindObjects()
+{
+    // Binds the texture to unit 0.
+    glDebug(gl->glActiveTexture(GL_TEXTURE0));
+    glDebug(m_texture->bind());
+
+    glDebug(m_vertexBuffer->bind());
+    glDebug(shaderProgram()->bind());
+}
+
+
+void Tilemap::releaseObjects()
+{
+    glDebug(m_texture->release());
+    glDebug(m_vertexBuffer->release());
+    glDebug(shaderProgram()->release());
+}
+
+
+void Tilemap::writeVertices()
+{
+    if (m_update)
+    {
+        glDebug(m_vertexBuffer->write(
+            GL_ZERO,
+            m_vertices.data(),
+            m_vertices.size() * priv::MapVertex::size())
+            );
+
+        m_update = false;
+    }
+}
+
+
+void Tilemap::modifyProgram()
+{
+    OpenGLShader* program = shaderProgram();
+
+    glDebug(program->setSampler(GL_TEXTURE0));
+    glDebug(program->setMvpMatrix(matrix(this)));
+    glDebug(program->setOpacity(opacity()));
+    glDebug(program->setWindowSize(renderTarget()->size()));
+}
+
+
+void Tilemap::modifyAttribs()
+{
+    glDebug(gl->glEnableVertexAttribArray(priv::MapVertex::xyAttrib()));
+    glDebug(gl->glEnableVertexAttribArray(priv::MapVertex::uvAttrib()));
+
+    glDebug(gl->glVertexAttribPointer(
+                priv::MapVertex::xyAttrib(),
+                priv::MapVertex::xyLength(),
+                GL_FLOAT,
+                GL_FALSE,
+                priv::MapVertex::size(),
+                priv::MapVertex::xyOffset()
+                ));
+
+    glDebug(gl->glVertexAttribPointer(
+                priv::MapVertex::uvAttrib(),
+                priv::MapVertex::uvLength(),
+                GL_FLOAT,
+                GL_FALSE,
+                priv::MapVertex::size(),
+                priv::MapVertex::uvOffset()
+                ));
+}
+
+
+void Tilemap::drawElements()
+{
+    // Renders all tiles at once.
+    glDebug(gl->glDrawArrays(
+                GL_TRIANGLES,
+                GL_ZERO,
+                m_vertices.size()
+                ));
+}
+
+
+bool Tilemap::setTiles(const QVector<uint>& tiles)
+{
+    removeAllTiles();
+
+    for (uint index : tiles)
+    {
+        if (!appendTile(index))
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+bool Tilemap::appendTile(uint tileIndex)
+{
+    if (m_currentX >= m_mapWidth)
+    {
+        m_currentX = 0;
+        m_currentY++;
+    }
+
+    if (m_currentY >= m_mapHeight)
+    {
+        // Out of map bounds.
+        return false;
+    }
+
+    priv::MapVertex t11, t12, t13, t21, t22, t23;
+
+    // Calculates the tile position from the tile index.
+    float uvX = ((tileIndex % m_setWidth) * m_tileWidth)  / (qreal) m_texture->width();
+    float uvY = ((tileIndex / m_setWidth) * m_tileHeight) / (qreal) m_texture->height();
+    float uvW = uvX + ((qreal) m_tileWidth  / m_texture->width());
+    float uvH = uvY + ((qreal) m_tileHeight / m_texture->height());
+    float xyX = m_currentX * m_tileWidth;
+    float xyY = m_currentY * m_tileHeight;
+    float xyW = xyX + m_tileWidth;
+    float xyH = xyY + m_tileHeight;
+
+    // Modifies the vertices.
+    t11.xy(xyX, xyY); t11.uv(uvX, uvY);
+    t12.xy(xyW, xyY); t12.uv(uvW, uvY);
+    t13.xy(xyX, xyH); t13.uv(uvX, uvH);
+    t21.xy(xyX, xyH); t21.uv(uvX, uvH);
+    t22.xy(xyW, xyY); t22.uv(uvW, uvY);
+    t23.xy(xyW, xyH); t23.uv(uvW, uvH);
+
+    m_vertices.push_back(t11);
+    m_vertices.push_back(t12);
+    m_vertices.push_back(t13);
+    m_vertices.push_back(t21);
+    m_vertices.push_back(t22);
+    m_vertices.push_back(t23);
+
+    m_currentX++;
+    m_update = true;
+
+    return true;
+}
+
+
+void Tilemap::removeAllTiles()
+{
+    m_currentX = 0;
+    m_currentY = 0;
+    m_vertices.clear();
+    m_update = true;
+}
